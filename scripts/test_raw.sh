d20b424ad0314f086482271e4d209073e76d94d4
Torsten Berger <herby@torstenberger.de>
2020-10-14 14:46:22 +0200

    changed path settings to fit new folderlayout

--- scripts/test_raw.sh
+++ scripts/test_raw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while :
 do
-	python /home/pi/gasmeter/test_raw.py >> /home/pi/gasmeter/input/test_raw.log
+	python /home/pi/git/gas_meter_rest_service/scripts/test_raw.py >> /home/pi/git/gas_meter_rest_service/input/test_raw.log
 	sleep 1
 done
